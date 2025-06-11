5bf7ec8b3344bbaf39805b395e1d82e28c6851c8
arman <arman.u.alam@gmail.com>
2025-06-11 21:24:48 +0600

    feat: run multiple instance of the backend server

--- services/user-service/src/index.ts
+++ services/user-service/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 app.get("/users", (req, res) => {
   res.json([
@@ -10,6 +10,6 @@ app.get("/users", (req, res) => {
   ]);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`User service running on port ${PORT}`);
 });
